fe5ebdaf6466d3e8077fc8fb4d0ee1d2e965600f
Sheetal <sborar12@gmail.com>
2021-06-23 19:21:37 +0800

    init commit

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def main_worker(gpu, args):
     else:
         start_epoch = 0
 
-    print(os.listdir('train_dataset/img'))
+    print(os.listdir(args.data / 'img'))
     dataset = torchvision.datasets.ImageFolder(args.data / 'img', Transform())
     sampler = torch.utils.data.distributed.DistributedSampler(dataset)
     assert args.batch_size % args.world_size == 0
