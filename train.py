ad96b564d5287cb1760c610923231dac0d43a824
Sheetal <sborar12@gmail.com>
2021-06-29 12:52:28 +0800

    wandb

--- train.py
+++ train.py
@@ -47,8 +47,9 @@ parser.add_argument('--print-freq', default=100, type=int, metavar='N',
 parser.add_argument('--checkpoint-dir', default='./checkpoint/', type=Path,
                     metavar='DIR', help='path to checkpoint directory')
 
-parser.add_argument('--device', default='cuda', type=str)
+parser.add_argument('--device', default='cpu', type=str)
 
+wandb.login(key='ed94033c9c3bebedd51d8c7e1daf4c6eafe44e09')
 wandb.init(project='barlow-twins', entity='sborar')
 config = wandb.config
 
