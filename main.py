93b86bffcff3f4407972f465e46b5b0ae3690621
Sheetal <sborar12@gmail.com>
2021-06-30 13:32:38 +0800

    increase batchsize

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ parser.add_argument('--workers', default=6, type=int, metavar='N',
                     help='number of data loader workers')
 parser.add_argument('--epochs', default=1000, type=int, metavar='N',
                     help='number of total epochs to run')
-parser.add_argument('--batch-size', default=8, type=int, metavar='N',
+parser.add_argument('--batch-size', default=32, type=int, metavar='N',
                     help='mini-batch size')
 parser.add_argument('--learning-rate-weights', default=0.2, type=float, metavar='LR',
                     help='base learning rate for weights')
