fb25118b27b46aeea58fcd44f5329c1db06bb034
Sheetal <sborar12@gmail.com>
2021-08-02 14:20:53 +0800

    changed image size from 128 to 256

--- train.py
+++ train.py
@@ -292,7 +292,7 @@ class Solarization(object):
 class Transform:
     def __init__(self):
         self.transform = transforms.Compose([
-            transforms.RandomResizedCrop(128, interpolation=Image.BICUBIC),
+            transforms.RandomResizedCrop(256, interpolation=Image.BICUBIC),
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomApply(
                 [transforms.ColorJitter(brightness=0.4, contrast=0.4,
@@ -307,7 +307,7 @@ class Transform:
                                  std=[0.229, 0.224, 0.225])
         ])
         self.transform_prime = transforms.Compose([
-            transforms.RandomResizedCrop(128, interpolation=Image.BICUBIC),
+            transforms.RandomResizedCrop(256, interpolation=Image.BICUBIC),
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomApply(
                 [transforms.ColorJitter(brightness=0.4, contrast=0.4,
